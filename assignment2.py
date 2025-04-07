9d68a210addcd4dd35bfa3318045325408887fc5
pujithaboddeti <125138716+pujithaboddeti@users.noreply.github.com>
2025-04-07 08:40:14 +0530

    assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,32 +1,4 @@
-# Basic String Manipulation:
 
-# Assign the string "Hello, World!" to a variable and print it.
-
-# Create a string "Python Programming" and extract the first five characters using slicing.
-
-# String Methods:
-
-# Given a string "python is fun", convert it to uppercase.
-
-# Replace "fun" with "awesome" in the string "Python is fun".
-
-# String Formatting:
-
-# Use f-string formatting to create a message like "My name is John, and I am 25 years old.", where the name and age are variables.
-
-# Given a price of 49.99, format it to display only two decimal places.
-
-# String Functions:
-
-# Count the occurrences of the letter 'o' in "Hello, how are you?".
-
-# Find the position of "world" in "Hello, world! Python is amazing.".
-
-# Reverse and Check:
-
-# Reverse the string "Python" using slicing.
-
-# Check if "madam" is a palindrome (a word that reads the same forward and backward).
 str1 = "Hello, World!"
 print(str1)
 str2 = "Python Programming"
